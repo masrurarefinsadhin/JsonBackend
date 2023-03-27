7875cab49664561acba9b05beaddf36391969312
masrurarefinsadhin <masrur.arefin.sadhin@g.bracu.ac.bd>
2023-03-27 14:58:06 +0600

    monday

--- Json-Book/src/main/java/com/jsonbook/Json/Book/controller/UserController.java
+++ Json-Book/src/main/java/com/jsonbook/Json/Book/controller/UserController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 
+import com.jsonbook.Json.Book.entity.SimpleUser;
 import com.jsonbook.Json.Book.service.UserRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -92,8 +93,11 @@ public class UserController {
     }
 
     @GetMapping("/list-of-users")
-    public List<SimpleUser> getAllListOfUsers(){
-        return userRepository.findAll().stream().map(user -> new SimpleUser(user.getId(), user.getFirstName()))
-                .collect(Collectors.toList());
+    public Map<Long,String> getAllListOfUsers(){
+        /*return userRepository.findAll().stream().map(user -> new SimpleUser(user.getId(), user.getFirstName()))
+                .collect(Collectors.toList());*/
+        return userRepository.findAll()
+                .stream().collect(Collectors.toMap(User::getId, User::getFirstName));
+
     }
 }
